73c78b811741fbf254887cc53cae18788ee20531
Takayuki Maeda <takoyaki0316@gmail.com>
2023-01-12 01:56:02 +0900

    use rust makefile

--- ext/rust_base64/src/lib.rs
+++ ext/rust_base64/src/lib.rs
@@ -8,6 +8,6 @@ fn foo(foo: &str) {}
 #[magnus::init]
 fn init() -> Result<(), Error> {
     let module = define_module("RustBase64")?;
-    // module.define_singleton_method("encode", function!(foo, 1))?;
+    module.define_singleton_method("encode", function!(foo, 1))?;
     Ok(())
 }
